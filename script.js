3813f8fb05022d3bc69ad02f0b92276c22b54c0b
Scott Davies <scottdavies1983@gmail.com>
2020-07-07 09:43:38 +0100

    static effect addrd

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let pageIndex = 0;
 
+var canvas = document.getElementById('canvas');
+
 let videoGallery = [
   {
     videos: `<div class="grid">
@@ -155,13 +157,19 @@ const gallery = document.querySelector(".video-container");
 const next = document.getElementById("nextbutton");
 const prev = document.getElementById("prevbutton");
 
-blink(2000);
+
+
+loop();
+static(900);
 
 next.addEventListener("click", (event) => {
-  blink(2000);
+  
+	
   document.body.scrollTop = document.documentElement.scrollTop = 780;
 
   pageIndex += 1;
+	
+	
 
   if (pageIndex < videoGallery.length) {
     //videos.length
@@ -170,17 +178,20 @@ next.addEventListener("click", (event) => {
     //document.body.style.backgroundColor = videoGallery[pageIndex].color;
   } else {
     pageIndex = 0;
+		
     gallery.innerHTML = "";
     gallery.innerHTML = videoGallery[pageIndex].videos;
     // document.body.style.backgroundColor = videoGallery[pageIndex].color;
   }
+	static(900);
 });
 
 prev.addEventListener("click", (event) => {
-  blink(2000);
+  
   document.body.scrollTop = document.documentElement.scrollTop = 780;
 
   pageIndex -= 1;
+	
   if (pageIndex > 0) {
     //videos.length
     gallery.innerHTML = "";
@@ -193,6 +204,7 @@ prev.addEventListener("click", (event) => {
     gallery.innerHTML = videoGallery[pageIndex].videos;
     // document.body.style.backgroundColor = videoGallery[pageIndex].color;
   }
+	static(900);
 });
 
 function invert() {
@@ -241,4 +253,87 @@ function longBlink(invert) {
   myVar = setTimeout(invert, 300);
 }
 
+function staticOn () {
+	
+	
+	let colorIndex = pageIndex;
+	
+	canvas.style.display = '';
+	
+	
+	canvas.style.backgroundColor = videoGallery[colorIndex].color;
+	
+}
+
+function staticOff() {
+	canvas.style.display = 'none';
+	
+}
+function static(time) {
+  staticOn();
+  myVar = setTimeout(staticOff, time);
+}
+
+ 
+
 //newTimer(blink);
+
+
+var canvas = document.getElementById('canvas'), 
+              
+        /* The getContext() method returns an object  
+        that provides methods and properties for  
+        drawing on the canvas. */ 
+        ctx = canvas.getContext('2d'); 
+  
+        /* Setting canvas width and height equal 
+        to window screen width and height. */ 
+        function resize() { 
+            canvas.width = window.innerWidth; 
+            canvas.height = window.innerHeight; 
+        } 
+        resize(); 
+        window.onresize = resize; 
+  
+        // Function to generate noise effect 
+        function generate_noise(ctx) { 
+            var w = ctx.canvas.width, 
+                h = ctx.canvas.height, 
+                  
+            /* This creates a new ImageData object 
+            with the specified dimensions(i.e canvas 
+            width and height). All pixels are set to 
+            transparent black (i.e rgba(0,0,0,0)). */ 
+            idata = ctx.createImageData(w, h),  
+                  
+            // Creating Uint32Array typed array 
+            buffer32 = new Uint32Array(idata.data.buffer),  
+            buffer_len = buffer32.length, 
+            i = 0 
+              
+            for (; i < buffer_len; i++) 
+                buffer32[i] = 
+                    ((255 * Math.random()) | 0) << 24; 
+              
+            /* The putImageData() method puts the 
+            image data (from a specified ImageData 
+            object) back onto the canvas. */ 
+            ctx.putImageData(idata, 0, 0);  
+        } 
+  
+        // Creating animation effect 
+   
+
+				var toggle = true; 
+        function loop() { 
+            toggle = !toggle; 
+            if (toggle) { 
+              
+                // Loop function is called each 
+                // time before next repaint 
+                requestAnimationFrame(loop);  
+                return; 
+            } 
+            generate_noise(ctx); 
+            requestAnimationFrame(loop); 
+					}; 
